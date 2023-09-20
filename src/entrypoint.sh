2643e8cc2b42265a19a96166c971a0efc9c13d5f
NathanInnes <nathaninnes001@gmail.com>
2023-09-20 10:36:25 +0100

    fix: entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,8 +4,8 @@ tail -f /var/fsd/log.txt /var/fsd/whazzup.txt &
 
 pip3 install --user requests
 
-# Start the fsd application in the background and redirect output to /var/fsd/fsd.log
+# Start the fsd application in the background
 nohup /var/fsd/fsd start
 
-# Start your Python script in the background (replace with your script name) and redirect output to /var/fsd/metar.log
-nohup python3 /var/fsd/metar.py
+# Start python script and output to metar.txt
+nohup python3 /var/fsd/metar.py >> /var/fsd/metar.txt 2>&1 &
