a4d242a3ff7b68618d4ec89122639e399934e5ba
NathanInnes <nathaninnes001@gmail.com>
2023-09-20 09:59:56 +0100

    fix: output logging

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,10 +3,10 @@
 pip3 install --user requests
 
 # Start the fsd application in the background and redirect output to /var/fsd/fsd.log
-nohup /var/fsd/fsd start >> /var/fsd/fsd.txt 2>&1 &
+nohup /var/fsd/fsd start
 
 # Start your Python script in the background (replace with your script name) and redirect output to /var/fsd/metar.log
-nohup python3 /var/fsd/metar.py >> /var/fsd/metar.txt 2>&1 &
+nohup python3 /var/fsd/metar.py
 
 # Keep the script running to keep the container running
 tail -f /dev/null
