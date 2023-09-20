5ddc164b31dbfdc6bbfc0ab5679aa1053000e2f7
NathanInnes <nathaninnes001@gmail.com>
2023-09-20 10:58:02 +0100

    fix: silly errors leading to metar not working

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-tail -f /var/fsd/log.txt /var/fsd/whazzup.txt &
-
 pip3 install --user requests
 
-# Start the fsd application in the background
-nohup /var/fsd/fsd start
+# Start the fsd application in the background and redirect output to /var/fsd/fsd.log
+nohup /var/fsd/fsd start >> /var/fsd/fsd.txt 2>&1 &
 
-# Start python script and output to metar.txt
+# Start your Python script in the background (replace with your script name) and redirect output to /var/fsd/metar.log
 nohup python3 /var/fsd/metar.py >> /var/fsd/metar.txt 2>&1 &
+
+# Keep the script running to keep the container running
+tail -f /var/fsd/fsd.txt /var/fsd/whazzup.txt
